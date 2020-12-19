fec567d7d3dfe41f898325f26ab466c04a057e81
Moritz <moritz.ahrens2@gmail.com>
2020-12-19 23:04:43 +0100

    Actually display hyic_event-carousel

--- hyic-plugin.php
+++ hyic-plugin.php
@@ -78,13 +78,78 @@ function create_block_hyic_blocks_init() {
     );
     */
  
-    register_block_type( 'create-block/gutenpride', array(
+    register_block_type( 'hyic/events-carousel', array(
         'apiVersion' => 2,
         'editor_script' => 'create-block-gutenpride-block-editor',
+        'render_callback' => 'gutenberg_examples_dynamic_render_callback'
         //'editor_style'  => 'create-block-gutenpride-block-editor',
         //'style'         => 'create-block-gutenpride-block',
     ) );
 }
 add_action( 'init', 'create_block_hyic_blocks_init' );
 
+function gutenberg_examples_dynamic_render_callback( $block_attributes, $content ) {
+    $recent_posts = wp_get_recent_posts( array(
+        'numberposts' => 3,
+        'post_status' => 'publish',
+        'post_type' => 'hyic_event'
+    ) );
+    if ( count( $recent_posts ) === 0 ) {
+        return 'No posts';
+    }
+    $result = '<div class="hyic-event-carousel-wrapper">';
+    foreach($recent_posts as $post) {
+        $registrationDeadline = date_create(get_post_meta( $post[ 'ID' ], '_hyic_event_registration_deadline', true ));
+        
+        $isAllDay = get_post_meta( $post[ 'ID' ], '_hyic_event_all_day', true )=='true';
+        $startDateString = get_post_meta( $post[ 'ID' ], '_hyic_event_start_date', true );
+        $startTimeString = get_post_meta( $post[ 'ID' ], '_hyic_event_start_time', true );
+        $endDateString = get_post_meta( $post[ 'ID' ], '_hyic_event_end_date', true );
+        $endTimeString = get_post_meta( $post[ 'ID' ], '_hyic_event_end_time', true );
+
+        $startDate = date_create($startDateString.' '.$startTimeString);
+        $endDate = date_create($endDateString.' '.$endTimeString);
+
+        $dateString = '';
+
+        if($isAllDay) {
+            if($startDateString == $endDateString) {
+                $dateString = 'Am '.date_format($startDate, 'd.m.Y');
+            } else {
+                $dateString = 'Vom '.date_format($startDate, 'd.m.').' bis '.date_format($endDate, 'd.m.Y');
+            }
+        } else {
+            if($startDateString == $endDateString) {
+                $dateString = 'Am '.date_format($startDate, 'd.m.Y').' von '.date_format($startDate, 'H:i').' bis '.date_format($endDate, 'H:i').' Uhr';
+            } else {
+                $dateString = 'Von '.date_format($startDate, 'd.m.Y H:i').' Uhr bis '.date_format($endDate, 'd.m.Y H:i').' Uhr';
+            }
+        }
+
+        $result .= sprintf(
+            "<div class='hyic-event-card'>
+                        <div class='hyic-event-card-image-wrapper'>
+                            <img src='%s'></img>
+                        </div>
+                        <div class='hyic-event-card-text-wrapper'>
+                            <span class='hyic-event-card-title'> %s </span>
+                            <span class='hyic-event-card-time'> %s </span>
+                            <span class='hyic-event-card-deadline'>Anmeldung bis: %s </span>
+                        </div>
+                        <a class='hyic-event-card-button' href=' %s '>
+                            <span>Jetzt anmelden</span>
+                        </a>
+                    </div>
+            ",
+            wp_get_attachment_image_url( get_post_thumbnail_id( $post['ID'] ), 'post-thumbnail' ),
+            esc_html( get_the_title( $post['ID'] ) ),
+            $dateString,
+            date_format($registrationDeadline, 'd.m.Y'),
+            esc_url( get_permalink( $post['ID'] ) ),
+        );
+    }
+    $result .= '/<div>';
+
+    return $result;
+}
 ?>
